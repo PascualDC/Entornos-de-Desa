94d383b7cb6ba563add34852509ade403bc77b3c
PascualDC <pascualdaroca678.alu@stemgranada.com>
2023-10-18 12:45:36 +0200

    Inicio

---
+++ RefactorizandoDias.py
@@ -0,0 +1,55 @@
+d1 = int(input("Introduce un día del mes: "))
+m1 = int(input("Introduce un mes: "))
+a1 = int(input("Introduce un año: "))
+
+if m1 == "febrero" and d1 > 28:
+	print("No es valido")
+elif (m1 == "septiembre" or m1 == "noviembre" or m1 == "abril" or m1 == "junio")and d1 > 30:
+	print("No es valido")
+elif d1 > 31:
+	print("No es valido")
+
+if a1 > 2025:
+	print("No es valido")
+	
+d2 = int(input("Introduce un día del mes: "))
+m2 = int(input("Introduce un mes: "))
+a2 = int(input("Introduce un año: "))
+
+if m2 == "febrero" and d2 > 28:
+	print("No es valido")
+elif (m2 == "septiembre" or m2 == "noviembre" or m2 == "abril" or m2 == "junio")and d2 > 30:
+	print("No es valido")
+elif d2 > 31:
+	print("No es valido")
+	
+if a2 > 2025:
+	print("No es valido")
+	
+d3 = int(input("Introduce un día del mes: "))
+m3 = int(input("Introduce un mes: "))
+a3 = int(input("Introduce un año: "))
+
+if m3 == "febrero" and d3 > 28:
+	print("No es valido")
+elif (m3 == "septiembre" or m3 == "noviembre" or m3 == "abril" or m3 == "junio")and d3 > 30:
+	print("No es valido")
+elif d3 > 31:
+	print("No es valido")
+	
+if a3 > 2025:
+	print("No es valido")
+	
+d4 = int(input("Introduce un día del mes: "))
+m4 = int(input("Introduce un mes: "))
+a4 = int(input("Introduce un año: "))
+
+if m4 == "febrero" and d4 > 28:
+	print("No es valido")
+elif (m4 == "septiembre" or m4 == "noviembre" or m4 == "abril" or m4 == "junio")and d4 > 30:
+	print("No es valido")
+elif d4 > 31:
+	print("No es valido")
+	
+if a4 > 2025:
+	print("No es valido")
